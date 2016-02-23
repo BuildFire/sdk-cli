bc2d7953a12343b39c5ba8d4495a20b86a1d0757
Daniel Hindi <Daniel@MadainCorp.com>
2016-02-22 19:14:41 -0800

    add snapshots

--- bin.js
+++ bin.js
@@ -11,7 +11,7 @@ var path = require ("path");
 ncp.limit = 32;
 
 
-var rmdir = function(dir) {
+function rmdir(dir) {
     var list = fs.readdirSync(dir);
     for(var i = 0; i < list.length; i++) {
         var filename = path.join(dir, list[i]);
@@ -36,7 +36,7 @@ var rmdir = function(dir) {
     }
 };
 
-var downloadRepo = function(repoName,url) {
+function downloadRepo(repoName,url) {
     if(!url)
         url="https://github.com/BuildFire/" + repoName + "/archive/master.zip";
     var localZipPath= "_" + repoName + ".zip";
@@ -79,6 +79,53 @@ var downloadRepo = function(repoName,url) {
     });
 };
 
+/// target : url or appid
+function snapshots(target){
+    var options = {
+        mode:0
+        ,delay: 10000
+        ,resolutions:[{
+            filename: 'images/iPhone4.png'
+            ,width: 640
+            ,height:960
+        },
+            {
+                filename: 'images/iPhone5.png'
+                ,width: 640
+                ,height:1136
+            },
+            {
+                filename: 'images/iPhone6.png'
+                ,width: 750
+                ,height:1334
+            },
+            {
+                filename: 'images/iPhone6Plus.png'
+                ,width: 1242
+                ,height:2208
+            },
+            {
+                filename: 'images/iPad.png'
+                ,width: 1536
+                ,height:2048
+            },{
+                filename: 'images/iPhonePro.png'
+                ,width: 2048
+                ,height:2732
+            }]
+    };
+
+    if (target.indexOf('http') != 0 )
+        options.appId=target;
+    else
+        options.url = target;
+
+    var s = require('./tools/screenshot.js');
+    s.capture(options,function(err,restults){
+        console.log('RESULTS ', restults.length);
+    });
+
+}
 /* args
 node.exe
 path
@@ -90,10 +137,13 @@ if(process.argv.length < 3 || ['-help','help','?','/?'].indexOf(process.argv[2].
     console.log('* create: this will download the latest BuildFire SDK in the current folder');
     console.log('* update: this will download the latest BuildFire SDK and update the current folder');
     console.log('* plugin [plugin name]: this will download the latest version of the indicated plugin in the current folder');
+    console.log('* snapshots [appId | url]: this will take pictures of the app home page or url requested at multiple resolutions');
     console.log('// many plugins are open source (MIT) feel free to Fork them on github http://github.com/buildfire');
 }
 else if(["create","update"].indexOf( process.argv[2].toLowerCase() ) >=0 )
     downloadRepo('sdk');
+else if(["snapshots","snapshot"].indexOf( process.argv[2].toLowerCase() ) >=0 )
+    snapshots(process.argv[3]);
 else if(process.argv[2].toLowerCase() =="plugin"  ) {
     if(process.argv.length < 4)
         console.error('* you forgot to indicate which plugin');
