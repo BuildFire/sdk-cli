3f4f842415462739182ad62789acb1360943a1bc
Daniel Hindi <Daniel@MadainCorp.com>
2016-02-22 22:11:09 -0800

    fix plugin load url

--- tools/screenshot.js
+++ tools/screenshot.js
@@ -8,14 +8,37 @@ module.exports = {
     /*
      * */
     capture: function(options, callback){
-        if(! options.url && options.appId)
+        if(!options.url && options.appId)
             options.url ="http://app.buildfire.com/app/index.html?appId=" + options.appId + "&mode=" + options.mode;
+
+        if(!options.url) {
+            console.error("no url provided");
+            callback("no url provided");
+            return;
+        }
+        else{
+            console.log('============= fix hash, search sequence ===============');
+            var hashPos = options.url.indexOf("#");
+            var searchPos = options.url.indexOf("?");
+            if( searchPos > 0 && hashPos > searchPos) {
+                var segments= options.url.split("?");
+                var newUrl = segments[0];
+                segments = segments[1].split("#");
+                options.hash=segments[1];
+                newUrl +=  "?" + segments[0];
+                console.warn("============= new url ", newUrl,'===================');
+                options.url = newUrl;
+            }
+
+        }
+
         var ph;
         function snap(res,callback){
             ph.createPage(function (page) {
 
                 page.set('viewportSize',res);
                 page.open(options.url, function (status) {
+                    page.evaluate('window.location.hash="' + options.hash + '"');
                     console.log(options.url , " opened? ", status);
 
                     setTimeout(function(){ /// we can change this to look for something in the dom to let you know its loaded
