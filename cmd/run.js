285f4e3c6835b9cabe18214f538fb18a5283ac7c
attila buturla <abuturla@buildfire.com>
2018-01-26 10:02:55 -0800

    WIP run plugin tester

--- cmd/run.js
+++ cmd/run.js
@@ -1,6 +1,31 @@
+var exec = require('child_process').exec;
 
 function run(args) {
 
+    var isWin = /^win/.test(process.platform),
+        cmd = 'http-server',
+        cmd2 = (isWin) ? 'start' : 'open';
+        cmd2 = cmd2 + ' http://localhost:8080/pluginTester/index.html';
+
+    //Note: callback is only run on error
+    exec(cmd, function(error, stdout, stderr) {
+        if(error){
+            console.log('error', error);
+            console.log('stdout', stdout);
+            console.log('stderr', stderr);
+            return;
+        }
+    });
+
+    console.log('Running plugin tester');
+
+    exec(cmd2, function(error, stdout, stderr) {
+        if(error){
+            console.log('error', error);
+            console.log('stdout', stdout);
+            console.log('stderr', stderr);
+        }
+    });
 }
 
 module.exports = run;
