c1a25f784fc11e16719726b67524cbd717c31370
AlexKvazos <contact@alexkvazos.com>
2018-08-08 11:53:57 -0500

    Better sdk run error and directory checking

--- cmd/run.js
+++ cmd/run.js
@@ -11,14 +11,12 @@ function run(args) {
     var defaultPort = 3030;
     if(Array.isArray(args) && args.length > 1) {
         if (isNaN(parseInt(args[1]))) {
-            console.error('\x1b[31m Error: Please provide a proper port number to run SDK');
+            console.error('\x1b[31m Error: Unknown argument ' +  args[1] + ', this command is to run SDK.');
             return;
         } else {
             defaultPort= parseInt(args[1]);
         }
     }
-
-    console.log({ defaultPort });
 
     getAvailablePort(defaultPort, function(port) {
         var app = express();
--- tools/isSdkDirectory.js
+++ tools/isSdkDirectory.js
@@ -5,8 +5,10 @@ function isSdkDirectory() {
 
   var data = fs.readdirSync(cwd);
 
+  data = data.map(item => item.toLowerCase());
+
   if (data.indexOf('scripts') > -1 &&
-      (data.indexOf('PluginTester') > -1 || data.indexOf('pluginTester') > -1) &&
+      data.indexOf('plugintester') &&
       data.indexOf('plugins') > -1 &&
       data.indexOf('package.json') > -1) {
     return true;
